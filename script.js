2a6555dca713f0f9381412be1a8753bfa864f8fd
ChrisKincaid <christophergkincaid@msn.com>
2023-07-23 15:41:44 -0500

    Code Clean up

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-    const navHBEL = document.querySelector('.nav-hamburger');
+// Hide nav items toggle -- Start
+
+const navHBEL = document.querySelector('.nav-hamburger');
     const hamburgerEL = document.querySelector('.hamburger');
 
     hamburgerEL.addEventListener('click', () => {
     navHBEL.classList.toggle('nav-hamburger--open');
     });
+
+// Hide nav items toggle -- End
 

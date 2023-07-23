c33dc6587d2d9e3b4f0485265a9b9a4bdacf9f0e
ChrisKincaid <christophergkincaid@msn.com>
2023-07-23 12:48:50 -0500

    Hamburger menu added

---
+++ script.js
@@ -0,0 +1,7 @@
+    const navHBEL = document.querySelector('.nav-hamburger');
+    const hamburgerEL = document.querySelector('.hamburger');
+
+    hamburgerEL.addEventListener('click', () => {
+    navHBEL.classList.toggle('nav-hamburger--open');
+    });
+
